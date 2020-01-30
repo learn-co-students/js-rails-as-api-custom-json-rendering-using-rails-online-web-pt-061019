729ed82ceb33767f2f45f1545b4da4823ad45338
Ingrid04 <ingridfmelgar@gmail.com>
2020-01-30 12:08:36 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,24 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # render json: Bird
+    # render json: {id: bird.id, name: bird.name, species: bird.species }
+    # just if we wanna render just this piece of data and not everything.
+    # or we can use this too: render json: bird.slice(:id, :name, :species)
+    render json: birds, only: [:id, :name, :species]
+    # we use this method above to work with arrays of hashes!
+    # or we can do it this way with exceptions to create the same result:
+    # render json: birds, except: [:created_at, :updated_at]
+    #   bird = Bird.find_by(id: params[:id])
+    # if bird
+    #   render json: { id: bird.id, name: bird.name, species: bird.species }
+    # else
+    #   render json: { message: 'Bird not found' }
+    # end
   end
 end
